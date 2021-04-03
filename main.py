a5bf3b862c507b0f8baddb2cf027adda0b4520ca
seolee0921 <seo050921@gmail.com>
2021-04-03 17:51:49 +0900

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+import cv2
+from openpyxl import Workbook
+
+image = cv2.imread("teemo minimalistic.jpg")
+wb = Workbook()
+ws = wb.active
+ws = wb.create_sheet("sample")
+
+n = 1
+for y in range(1, image.shape[0]):
+    for x in range(image.shape[1]):
+        ws.cell(row=y, column=x, value=n)
+        n += 1
+
+wb.save("sample.xlsx")
