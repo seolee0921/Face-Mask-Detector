3468419dcecac87cb9723b30ae9262e129199f6c
seolee0921 <seo050921@gmail.com>
2021-04-10 16:10:48 +0900

    make gui first step

--- main.py
+++ main.py
@@ -1,15 +1,40 @@
-import cv2
-from openpyxl import Workbook
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget, QDesktopWidget, QPushButton, QBoxLayout
+from PyQt5.QtGui import QIcon
+from win32api import GetSystemMetrics
+from PyQt5.QtCore import QCoreApplication
+import time
 
-image = cv2.imread("teemo minimalistic.jpg")
-wb = Workbook()
-ws = wb.active
-ws = wb.create_sheet("sample")
+class Myapp(QWidget):
 
-n = 1
-for y in range(1, image.shape[0]):
-    for x in range(1, image.shape[1]):
-        ws.cell(row=y, column=x, value=n)
-        n += 1
+    def __init__(self):
+        super().__init__()
+        self.initUI()
 
-wb.save("sample.xlsx")
+    def initUI(self):
+
+        self.setWindowTitle('Face_Mask_Detector')
+        self.resize(GetSystemMetrics(0) / 3 * 2, GetSystemMetrics(1) / 3 * 2)
+        self.center()
+        self.setWindowIcon(QIcon('python.png'))
+
+            self.AddButton()
+            self.show()
+
+
+    def AddButton(self):
+        btn = QPushButton('Push', self)
+        btn.setGeometry(self.width() / 2, self.height() / 2, self.width() / 20, self.height() / 20)
+        btn.clicked.connect(QCoreApplication.instance().quit)
+
+    def center(self):
+        qr = self.frameGeometry()
+        cp = QDesktopWidget().availableGeometry().center()
+        qr.moveCenter(cp)
+        self.move(qr.topLeft())
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = Myapp()
+    sys.exit(app.exec_())
