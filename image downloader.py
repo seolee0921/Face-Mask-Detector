05b839f078deff9ae9202f0ffc56abf4bab88245
seolee0921 <seo050921@gmail.com>
2021-04-24 16:31:07 +0900

    draw_line

---
+++ image downloader.py
@@ -0,0 +1,44 @@
+from urllib.request import Request, urlopen
+import json
+import os
+
+def image_download(url, filepath):
+    request = Request(url)
+    response = urlopen(request)
+    image_data = response.read()
+    file = open(filepath, 'wb')
+    file.write(image_data)
+    file.close()
+    print(f'{url}로 부터 {filepath}에 다운로드 완료')
+
+mask_url = 'https://raw.githubusercontent.com/prajnasb/observations/master/mask_classifier/Data_Generator/images/blue-mask.png'
+image_download(mask_url, 'mask.png')
+exit()
+
+# save_folder = 'data/without_mask/'
+# api_url = 'https://api.github.com/repos/prajnasb/observations/contents/experiements/data/without_mask?ref=master'
+#
+# header = {'User-Agent': 'Mozilla/5.0'}
+#
+# request = Request(api_url, headers=header)
+# response = urlopen(request)
+# directory_bytes = response.read()
+# directory_str = directory_bytes.decode('utf-8')
+#
+# contents = json.loads(directory_str)
+
+# for i in range(len(contents)):
+#     content = contents[i]
+#     request = Request(content['download_url'], headers=header)
+#     response = urlopen(request)
+#     data = response.read()
+#
+#     if not os.path.exists('data'):
+#         os.mkdir('data')
+#
+#     if not os.path.exists('data/without_mask'):
+#         os.mkdir('data/without_mask')
+#
+#     file = open(save_folder + content['name'], 'wb')
+#     file.write(data)
+#     print(f"다운로드 {i}번째 완료 : {str(len(contents))}, {content['name']}")
